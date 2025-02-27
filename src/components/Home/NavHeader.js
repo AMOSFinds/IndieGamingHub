69fb418d930433b0c9264f69a14720079da350f9
Amos <amogetswe.mashele@gmail.com>
2025-02-27 22:35:00 +0200

    Fix ESLint error in NavHeader.js by importing useState

--- src/components/Home/NavHeader.js
+++ src/components/Home/NavHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase/firebase-config";
 import { useAuth } from "../Authentication/AuthContext";
